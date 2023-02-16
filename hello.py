6d3c869fafaee92fb1bdc8a79d1db88a6fa15da4
Erlit <erlit007@gmail.com>
2023-02-16 22:17:26 +0300

    Add SQLAlchemy

--- hello.py
+++ hello.py
@@ -1,10 +1,35 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, StringField
+from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 DEBUG = bool(os.getenv('DEBUG', True))
 PORT = int(os.getenv('PORT', 8080))
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my hot pot secret key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+db = SQLAlchemy(app)
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(200), nullable=False)
+    email = db.Column(db.String(200), nullable=False, unique=True)
+    date_added = db.Column(db.DateTime, default=datetime.utcnow())
+
+# Create a string
+def __repr__(self):
+    return f'{self.name}'
+
+
+
+# Create a Form Class
+class NamerForm(FlaskForm):
+    name = StringField('What is our name?', validators=[DataRequired()])
+    submit = SubmitField('Push')
 
 @app.route('/')
 def index():
@@ -19,15 +44,28 @@ def index():
 def user(name):
     return render_template('user.html', user_name=name)
 
-#Invalid URL - 404
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
+@app.route('/name', methods = ['GET', 'POST'])
+def name():
+    name = None
+    form = NamerForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash('Success')
+    return render_template('name.html', name=name, form=form)
+
+
+
 
 #Bad Request - 500
 @app.errorhandler(500)
 def page_not_found(e):
     return render_template('500.html'), 500
+#Invalid URL - 404
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
