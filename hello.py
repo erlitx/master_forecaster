68bb5834b94be27cf10c303ed8d740f6b54da297
Erlit <erlit007@gmail.com>
2023-02-18 21:10:52 +0300

    Codemy #13 completed

--- hello.py
+++ hello.py
@@ -6,6 +6,7 @@ from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 DEBUG = bool(os.getenv('DEBUG', True))
 PORT = int(os.getenv('PORT', 8080))
@@ -30,6 +31,21 @@ class Users(db.Model):
     email = db.Column(db.String(200), nullable=False, unique=True)
     favorite_color = db.Column(db.String(200))
     date_added = db.Column(db.DateTime, default=datetime.utcnow())
+    # Do some password stuff!
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
     # Create a string
     def __repr__(self):
         return f'<Name {self.name}>'
